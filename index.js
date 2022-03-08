7d8a310f5366e45d163992a580b3d8f2a2d5eafc
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-07 21:52:20 -0500

    changed game to index

---
+++ index.js
@@ -0,0 +1,11 @@
+"use script"
+
+// Date: 03/07/2022 //
+// Project: RPSLS //
+// Customer: devCodeCamp //
+// Developers: Dustin Barker && Parker Hadley //
+
+
+const Game = require("./game");
+let game = new Game();
+game.runGame();
