479699f2f21e745780bfa361fdff4fbd07c59884
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-07 22:51:24 -0500

    Added greeting display to game.js

---
+++ game.js
@@ -0,0 +1,31 @@
+"use script"
+
+// Date: 03/07/2022 //
+// Project: RPSLS //
+// Customer: devCodeCamp //
+// Developers: Dustin Barker && Parker Hadley //
+
+
+// const prompt = require('prompt-sync')();
+// const { Player, Human, AI } = require('./players');
+
+// Class Game {
+//     constructor() 
+
+    
+    displayIntroduction(); {
+        console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+        console.log("Rules(below):")
+        console.log("Rock breaks Scissors")
+        console.log("Rock crushes Lizard")
+        console.log("Paper disproves Spock")
+        console.log("Paper covers Rock")
+        console.log("Scissors decapitate Lizard")
+        console.log("Scissors cut Paper")
+        console.log("Lizard poisons Spock")
+        console.log("Lizard eats Paper")
+        console.log("Spock pockets the Scissors")
+        console.log("Spock vaporizes Rock")
+    }   
+    
+    displaySetup
---
+++ players.js
@@ -0,0 +1,7 @@
+"use script"
+
+// Date: 03/07/2022 //
+// Project: RPSLS //
+// Customer: devCodeCamp //
+// Developers: Dustin Barker && Parker Hadley //
+
